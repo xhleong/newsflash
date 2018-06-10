371af48ebebcc00e41b414b93bd1600db3e3c177
XinHui <xhleong@hotmail.com>
2018-06-10 16:14:27 +0800

    fixed current user

--- lib/news_api.rb
+++ lib/news_api.rb
@@ -7,8 +7,8 @@ class NewsApi
   base_uri 'newsapi.org'
 
 
-  def get_sources
-    response = self.class.get("/v2/sources?apiKey=#{ENV['NEWS_API_KEY']}")
+  def self.get_sources
+    response = self.get("/v2/sources?apiKey=#{ENV['NEWS_API_KEY']}")
     if response.success?
       response
     else
